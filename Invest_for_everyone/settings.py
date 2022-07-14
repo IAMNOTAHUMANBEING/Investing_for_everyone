0a05cc9e49eed0ada00626d3f3135802b9d1fb52
Human <60809568+IAMNOTAHUMANBEING@users.noreply.github.com>
2022-07-14 17:43:33 +0900

    CORS

--- Invest_for_everyone/settings.py
+++ Invest_for_everyone/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'markdownx',
     'fontawesomefree',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://144.24.92.23",
 ]
 
 ROOT_URLCONF = 'Invest_for_everyone.urls'
